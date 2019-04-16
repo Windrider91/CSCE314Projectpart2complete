2a1cba68ce4b58a93c29e428eca19c3146240df2
Glen-TAMU <hordemann@tamu.edu>
2019-04-16 16:14:05 -0500

    fixed spacing.

--- javafit/src/Config.java
+++ javafit/src/Config.java
@@ -5,5 +5,5 @@ public class Config {
   public final static String APPLICATIONNAME = "JavaFit: Muscles for Computer Scientists!";
   public final static String WORKOUTFILE = "workouts.csv";
   public final static String WORKOUTFORMATFILE = "workoutformats.csv";
-  public enum MuscleGroup {UPPERBODY, LOWERBODY, WHOLEBODY}
+  public enum MuscleGroup {UPPERBODY, LOWERBODY, WHOLEBODY} 
 }
